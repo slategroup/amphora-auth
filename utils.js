b3670fae7ef5c3bbb34098fb98956717f7b074b2
Manuel Emilio Urena <manuel.ureh@hotmail.com>
2019-03-07 10:44:45 -0500

    Adds condition for password validation

--- utils.js
+++ utils.js
@@ -92,7 +92,7 @@ function verify(properties) {
         .then(data => {
           return db.put(uid, JSON.stringify(data))
             .then(() => {
-              if (!isValidPassword(data, password)) {
+              if (password && !isValidPassword(data, password)) {
                 return done(null, false, { message: 'Invalid Password' });
               }
 
@@ -105,7 +105,7 @@ function verify(properties) {
       // already authenticated. just grab the user data
       return db.get(uid)
         .then(data => {
-          if (!isValidPassword(data, password)) {
+          if (password && !isValidPassword(data, password)) {
             return done(null, false, { message: 'Invalid Password' });
           }
 
